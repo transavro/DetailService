b99e005c7dddf987d49e3c8a99017faae52f2c96
nayan <nvmakasare@gmail.com>
2020-01-23 18:30:37 +0530

    first commit

---
+++ apihandler/handler.go
@@ -0,0 +1,193 @@
+package apihandler
+
+import (
+	pb "github.com/transavro/DetialService/proto"
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+
+
+type movieTile struct {
+	Posters struct {
+		Landscape        []string      `json:"landscape"`
+		Portrait         []string      `json:"portrait"`
+		Backdrop         []string      `json:"backdrop"`
+	} `json:"posters"`
+	Content struct {
+		Source       string   `json:"source"`
+		DetailPage 	 bool	  `json:"detailPage"`
+		Package      string   `json:"package"`
+		Target       []string `json:"target"`
+	} `json:"content"`
+	Metadata struct {
+		Title           string      `json:"title"`
+		Synopsis        string      `json:"synopsis"`
+		Rating          float64         `json:"rating"`
+		Runtime         string      `json:"runtime"`
+		Year            string      `json:"year"`
+		Cast            []string    `json:"cast"`
+		Directors       []string    `json:"directors"`
+		Genre           []string    `json:"genre"`
+		Categories      []string    `json:"categories"`
+		Languages       []string    `json:"languages"`
+	} `json:"metadata"`
+	Buttons []struct {
+		Title       string `json:"title"`
+		Icon        string `json:"icon"`
+		PackageName string `json:"packageName"`
+		Action      string `json:"action"`
+	} `json:"buttons"`
+}
+
+
+type Server struct {
+	TileCollection *mongo.Collection
+}
+
+func(s *Server) GetDetailInfo(ctx context.Context, tileInfo *pb.TileInfoRequest) (*pb.DetailTileInfo, error){
+	findFilter := bson.D{{"ref_id", tileInfo.TileId}}
+	findResult := s.TileCollection.FindOne(ctx, findFilter)
+
+	var tile movieTile
+	err := findResult.Decode(&tile)
+	if err != nil {
+		return nil, err
+	}
+
+	//making Tile detail Info
+	var detailTileInfo pb.DetailTileInfo
+	detailTileInfo.Title = tile.Metadata.Title
+	if len(tile.Posters.Backdrop) > 0 {
+		detailTileInfo.BackDrop = tile.Posters.Backdrop[0]
+	}
+	if len(tile.Posters.Portrait) > 0 {
+		detailTileInfo.Portrait = tile.Posters.Portrait[0]
+	}
+	metaSet := make(map[string]string)
+
+	// creating pipes for mongo aggregation
+	myStages := mongo.Pipeline{}
+
+
+	myStages = append(myStages,bson.D{{"$match", bson.D{{"ref_id", bson.D{{"$ne", tileInfo.GetTileId()}}}}}},)
+
+
+	// fetching related content
+	//myStages = append(myStages , bson.D{{"$match", bson.D{{"content.publishState", true}}}})
+
+	if len(tile.Metadata.Categories) > 0 {
+		myStages = append(myStages, bson.D{{"$match", bson.D{{"metadata.categories", bson.D{{"$in", tile.Metadata.Categories}}}}}})
+	}
+
+	if len(tile.Metadata.Genre) > 0 {
+		metaSet["genre"] = strings.Join(tile.Metadata.Genre, ",")
+		myStages = append(myStages, bson.D{{"$match", bson.D{{"metadata.genre", bson.D{{"$in", tile.Metadata.Genre}}}}}})
+	}
+
+	if len(tile.Metadata.Languages) > 0 {
+		metaSet["language"] = strings.Join(tile.Metadata.Languages, ",")
+		myStages = append(myStages, bson.D{{"$match", bson.D{{"metadata.languages", bson.D{{"$in", tile.Metadata.Languages}}}}}})
+	}
+
+
+	if len(tile.Metadata.Runtime) > 0  {
+		metaSet["runtime"] = tile.Metadata.Runtime
+	}
+	if len(tile.Metadata.Year) > 0 {
+		metaSet["year"] = tile.Metadata.Year
+	}
+	if tile.Metadata.Rating != 0 {
+		metaSet["rating"] = fmt.Sprint(tile.Metadata.Rating)
+	}
+	if len(tile.Content.Source) > 0 {
+		metaSet["source"] = tile.Content.Source
+	}
+
+	if(len(tile.Metadata.Cast) > 0){
+		metaSet["cast"] = strings.Join(tile.Metadata.Cast, ",")
+	}
+
+	if(len(tile.Metadata.Directors) > 0){
+		metaSet["director"] = strings.Join(tile.Metadata.Directors, ",")
+	}
+
+	detailTileInfo.Metadata = metaSet
+	detailTileInfo.Synopsis = tile.Metadata.Synopsis
+	detailTileInfo.Target = tile.Content.Target
+
+	var buttons []*pb.Button
+	for _, v := range tile.Buttons {
+		var button pb.Button
+		button.Title = v.Title
+		button.PackageName = v.PackageName
+		button.Action = v.Action
+		button.Icon = v.Icon
+		buttons = append(buttons, &button)
+	}
+	detailTileInfo.Button = buttons
+
+	myStages = append(myStages,bson.D{{"$sort", bson.D{{"created_at", -1}, {"updated_at", -1}, {"metadata.year", -1}}}},)
+
+	myStages = append(myStages,bson.D{{"$limit", 15}})
+
+	myStages = append(myStages, bson.D{{"$project", bson.D{
+		{"_id", 0},
+		{"ref_id", 1},
+		{"metadata.title", 1},
+		{"posters.landscape", 1},
+		{"posters.portrait", 1},
+		{"content.package", 1},
+		{"content.source", 1},
+		{"content.target", 1},
+		{"created_at", 1},
+		{"content.detailPage", 1},
+		{"metadata.releaseDate", 1}}}} )
+
+	// creating aggregation query
+	cur, err := s.TileCollection.Aggregate(ctx, myStages)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var relatedTiles []*pb.ContentTile
+	defer cur.Close(ctx)
+	for cur.Next(ctx) {
+		var mTile movieTile
+		// converting curors to movieTiles
+		err := cur.Decode(&mTile)
+		if err != nil {
+			return nil,err
+		}
+
+		var contentTile pb.ContentTile
+
+		contentTile.Title = mTile.Metadata.Title
+		contentTile.IsDetailPage = mTile.Content.DetailPage
+		if len(mTile.Posters.Portrait) > 0 {
+			contentTile.Portrait = mTile.Posters.Portrait[0]
+		}
+
+		if len(mTile.Posters.Landscape) > 0 {
+			contentTile.Poster = mTile.Posters.Landscape[0]
+		}
+
+		contentTile.ContentId = cur.Current.Lookup("ref_id").StringValue()
+		contentTile.Target = mTile.Content.Target
+		contentTile.TileType = pb.TileType_ImageTile
+		contentTile.PackageName = mTile.Content.Package
+		relatedTiles = append(relatedTiles, &contentTile)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(relatedTiles), func(i, j int) { relatedTiles[i], relatedTiles[j] = relatedTiles[j], relatedTiles[i] })
+	detailTileInfo.ContentTile = relatedTiles
+
+	return &detailTileInfo, nil
+}
---
+++ server.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"github.com/transavro/DetialService/apihandler"
+	pb "github.com/transavro/DetialService/proto"
+	"context"
+	"errors"
+	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/bsoncodec"
+	"go.mongodb.org/mongo-driver/bson/bsonrw"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"log"
+	"net"
+	"net/http"
+	"reflect"
+	"strings"
+	"time"
+)
+
+
+// private type for Context keys
+type contextKey int
+
+const (
+	clientIDKey contextKey = iota
+)
+
+const (
+	defaultHost = "mongodb://nayan:tlwn722n@cluster0-shard-00-00-8aov2.mongodb.net:27017,cluster0-shard-00-01-8aov2.mongodb.net:27017,cluster0-shard-00-02-8aov2.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+	developmentMongoHost = "mongodb://dev-uni.cloudwalker.tv:6592"
+	schedularMongoHost = "mongodb://localhost:27017"
+	schedularRedisHost = ":6379"
+)
+
+type nullawareStrDecoder struct{}
+
+func (nullawareStrDecoder) DecodeValue(dctx bsoncodec.DecodeContext, vr bsonrw.ValueReader, val reflect.Value) error {
+	if !val.CanSet() || val.Kind() != reflect.String {
+		return errors.New("bad type or not settable")
+	}
+	var str string
+	var err error
+	switch vr.Type() {
+	case bsontype.String:
+		if str, err = vr.ReadString(); err != nil {
+			return err
+		}
+	case bsontype.Null: // THIS IS THE MISSING PIECE TO HANDLE NULL!
+		if err = vr.ReadNull(); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("cannot decode %v into a string type", vr.Type())
+	}
+
+	val.SetString(str)
+	return nil
+}
+
+
+func credMatcher(headerName string) (mdName string, ok bool) {
+	if headerName == "Login" || headerName == "Password" {
+		return headerName, true
+	}
+	return "", false
+}
+
+// authenticateAgent check the client credentials
+func authenticateClient(ctx context.Context, s *apihandler.Server) (string, error) {
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		clientLogin := strings.Join(md["login"], "")
+		clientPassword := strings.Join(md["password"], "")
+		if clientLogin != "nayan" {
+			return "", fmt.Errorf("unknown user %s", clientLogin)
+		}
+		if clientPassword != "makasare" {
+			return "", fmt.Errorf("bad password %s", clientPassword)
+		}
+		log.Printf("authenticated client: %s", clientLogin)
+		return "42", nil
+	}
+	return "", fmt.Errorf("missing credentials")
+}
+
+func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	s, ok := info.Server.(*apihandler.Server)
+	if !ok {
+		return nil, fmt.Errorf("unable to cast the server")
+	}
+	clientID , err := authenticateClient(ctx, s)
+	if err != nil {
+		return nil, err
+	}
+	ctx = context.WithValue(ctx, clientIDKey, clientID)
+	return handler(ctx, req)
+}
+
+func startGRPCServer(address string, server apihandler.Server) error {
+	// create a listener on TCP port
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}  // create a server instance
+	if err != nil {
+		return err
+	}
+
+	// attach the Ping service to the server
+	grpcServer := grpc.NewServer()  // attach the Ping service to the server
+	pb.RegisterDetailPageServiceServer(grpcServer, &server)  // start the server
+	//log.Printf("starting HTTP/2 gRPC server on %s", address)
+	if err := grpcServer.Serve(lis); err != nil {
+		return fmt.Errorf("failed to serve: %s", err)
+	}
+	return nil
+}
+
+func startRESTServer(address, grpcAddress string) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	mux := runtime.NewServeMux(runtime.WithIncomingHeaderMatcher(credMatcher))
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}  // Register ping
+	err := pb.RegisterDetailPageServiceHandlerFromEndpoint(ctx, mux, grpcAddress, opts)
+	if err != nil {
+		return fmt.Errorf("could not register service Ping: %s", err)
+	}
+
+	log.Printf("starting HTTP/1.1 REST server on %s", address)
+	http.ListenAndServe(address, mux)
+	return nil
+}
+
+func getMongoCollection(dbName, collectionName, mongoHost string )  *mongo.Collection {
+
+	// Register custom codecs for protobuf Timestamp and wrapper types
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	mongoClient, err :=  mongo.Connect(ctx, options.Client().ApplyURI(mongoHost), options.Client().SetRegistry(bson.NewRegistryBuilder().
+		RegisterDecoder(reflect.TypeOf(""), nullawareStrDecoder{}).
+		Build(),))
+
+	if err != nil {
+		log.Println("Error while making collection obj ")
+		log.Fatal(err)
+	}
+	return mongoClient.Database(dbName).Collection(collectionName)
+}
+
+func main()  {
+	serverhandler := initializeProcess();
+
+	grpcAddress := fmt.Sprintf(":%d",  7767)
+	restAddress := fmt.Sprintf(":%d", 7768)
+
+
+	// fire the gRPC server in a goroutine
+	go func() {
+		err := startGRPCServer(grpcAddress, serverhandler)
+		if err != nil {
+			log.Fatalf("failed to start gRPC server: %s", err)
+		}
+	}()
+
+	// fire the REST server in a goroutine
+	go func() {
+		err := startRESTServer(restAddress, grpcAddress)
+		if err != nil {
+			log.Fatalf("failed to start gRPC server: %s", err)
+		}
+	}()
+
+	// infinite loop
+	select {}
+}
+
+func initializeProcess() apihandler.Server  {
+	tileCollection := getMongoCollection("cwtx2devel", "tiles", developmentMongoHost);
+	return apihandler.Server{TileCollection:tileCollection}
+}
+
